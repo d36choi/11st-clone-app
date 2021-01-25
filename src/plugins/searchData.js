8750789be9b917b9f9a808849335c9ce72b29b61
d36choi <yuio1126@gmail.com>
2021-01-25 16:51:52 +0900

    intall 을 install 로 수정

--- src/plugins/searchData.js
+++ src/plugins/searchData.js
@@ -1,9 +1,8 @@
 
 import axios from "axios";
 
-
 export default {
-    intall (Vue) {
+    install (Vue) {
         Vue.prototype.$search = async ({searchText}) => {
             const {data} = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/search?apikey=${1216}&searchText=${searchText}`)
             // api key 1216
