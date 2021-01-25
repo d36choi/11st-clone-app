b8bfd2a1cffab26174171df742f781719bb36091
d36choi <yuio1126@gmail.com>
2021-01-25 17:30:39 +0900

    error fixed

--- src/components/LNB.vue
+++ src/components/LNB.vue
@@ -1,15 +1,43 @@
-<!--left nav bar-->
 <template>
   <div>
-    <nav :class="{show: isShowLNB}">
+    <nav
+      v-if="done"
+      :class="{ show: isShowLNB }">
       <div class="user">
         <a href="javascript:void(0)">로그인</a>
+        <div class="flex-space"></div>
         <div
           class="close-nav"
           @click="offNav"></div>
       </div>
+      <div class="container">
+        <!--GROUP-->
+        <div class="group categories">
+          <h3 class="group__title">
+            {{ navigations.categories.title }}
+          </h3>
+          <ul class="group__list">
+            <li
+              v-for="(item1, index) in navigations.categories.list"
+              :key="item1.name">
+              <div class="category-icon"></div>
+              {{ item1.name }}
+              <ul class="depth">
+                <li
+                  v-for="item2 in item1.list"
+                  :key="item2.name">
+                  <a :href="item2.href">
+                    {{ item2.name }}
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    <!--    background-->
+
+    <!--BACKGROUND-->
     <div
       v-if="isShowLNB"
       class="nav-bg"
@@ -17,16 +45,32 @@
   </div>
 </template>
 
-
 <script>
+import axios from 'axios'
+
 export default {
+  data () {
+    return {
+      navigations: {},
+      done: false
+    }
+  },
   computed: {
     isShowLNB () {
-      // this.$store.state.namespace.state
+      // this.$store.state.네임스페이스.상태
       return this.$store.state.navigation.isShowLNB
     }
   },
+  created () {
+    this.init()
+  },
   methods: {
+    async init () {
+      this.navigations = await this.$fetch({
+        requestName: 'navigations'
+      })
+      this.done = true
+    },
     offNav () {
       this.$store.dispatch('navigation/offNav')
     }
@@ -35,47 +79,64 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  nav {
-    width: 300px;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 99;
-    background-color: #f6f6f6;
-    transition: transfrorm .4s;
-    transform: translateX(-300px);
-    &.show {
-      transform: translateX(0px);
-    }
-    .user {
-      a {
-        font-size: 20px;
-        font-weight: 700;
-        text-decoration: none;
-        &:hover {
-          text-decoration: underline;
-        }
+nav {
+  width: 300px;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 99;
+  background-color: #f6f6f6;
+  transition: transform .4s;
+  transform: translateX(-300px);
+  &.show {
+    transform: translateX(0);
+  }
+  .user {
+    height: 70px;
+    padding: 0 25px;
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+    a {
+      font-size: 20px;
+      font-weight: 700;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
       }
-      .close-nav {
-        width: 36px;
-        height: 36px;
-        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
-        background-position: -261px -203px;
-        background-size: 363px;
-        cursor: pointer;
+    }
+    .flex-space {
+      flex-grow: 1;
+    }
+    .close-nav {
+      width: 36px;
+      height: 36px;
+      background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+      background-position: -261px -203px;
+      background-size: 363px;
+      cursor: pointer;
+    }
+  }
+  .container {
+    .group {
+      &__title {
+
+      }
+      &__list {
 
       }
     }
   }
-  .nav-bg {
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background-color: rgba(#000, .2);
-    z-index: 98;
-  }
+}
+.nav-bg {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(#000, .2);
+  z-index: 98;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 import searchData from './plugins/searchData'
-
+import fetchData from "./plugins/fetchData";
 Vue.use(searchData)
-
+Vue.use(fetchData)
 new Vue({
     el: '#app',
-    store: store,
+    store,
     render: createElement => createElement(App),
 
 })
