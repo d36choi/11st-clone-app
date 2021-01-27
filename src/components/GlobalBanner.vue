e35154ff6e973e7b4830b86e8b637e109c3cf364
d36choi <yuio1126@gmail.com>
2021-01-27 15:05:41 +0900

    feat: Add GlobalBanner.vue components and make html element tree

---
+++ src/components/GlobalBanner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="global-banner">
+    <div class="ad"></div>
+    <a
+      href=""
+      class="wing wing--vertical">
+      <div class="wing__inner">
+        <div class="text"></div>
+        <div class="thumbnail"></div>
+      </div>
+    </a>
+    <div class="wing-outer">
+      <a
+        href=""
+        class="wing wing--horizontal">
+        <div class="wing__inner">
+          <div class="text">
+            <div class="name"></div>
+            <div class="price"></div>
+          </div>
+          <div class="thumbnail"></div>
+        </div>
+      </a>
+    </div>
+    <div class="to-top"></div>
+  </div>
+</template>
+
+<script>
+import scrollTo from 'scroll-to'
+
+export default {
+  
+}
+</script>
