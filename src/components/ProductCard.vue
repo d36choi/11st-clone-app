9a48bb31cef06d980033128b2a0c46222bbc4b56
d36choi <yuio1126@gmail.com>
2021-01-27 11:32:59 +0900

    feat: Create ProductCard.vue

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    href=""
+    class="product-card"></a>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style scoped lang="scss">
+.product-card {
+  display: block;
+  width: 394px;
+  height: 368px;
+  background-color: #fff;
+  border: 1px solid #ececec;
+  border-radius: 6px;
+  overflow: hidden;
+  box-shadow: 0 2px 10px rgba(#000, .06);
+}
+
+</style>
