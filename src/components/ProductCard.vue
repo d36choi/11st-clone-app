1ca6d72d284815b1dabbc09f986dce85bda7665d
d36choi <yuio1126@gmail.com>
2021-01-27 12:01:54 +0900

    feat: Set ProductCard.vue html element architecture

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,11 +1,50 @@
 <template>
   <a
-    href=""
-    class="product-card"></a>
+    class="product-card">
+    <div></div>
+    <div class="contents">
+      <div class="flag"></div>
+      <div class="name"></div>
+      <div class="prices">
+        <div class="discount-rate">
+          <span class="value"></span>
+          <span class="unit"></span>
+        </div>
+        <div class="price">
+          <span class="value"></span>
+          <span class="unit"></span>
+        </div>
+        <div class="list-price">
+          <span class="value"></span>
+          <span class="unit"></span>
+        </div>
+        <div class="options">
+          <div class="free-shipping"></div>
+          <div class="accumulation"><span></span></div>
+          <span class="flex-space"></span>
+          <div class="count"></div>
+        </div>
+
+      </div>
+      <div class="coupons">
+        <a
+          href=""
+          class="coupon">
+          <div class="coupon__amount"></div>
+          <div class="coupon__name"></div>
+        </a>
+      </div>
+
+    </div>
+  </a>
 </template>
 
 
 <script>
+
+import numeral from 'numeral'
+import _kebabCase from 'lodash/kebabCase'
+
 
 </script>
 
@@ -13,12 +52,13 @@
 <style scoped lang="scss">
 .product-card {
   display: block;
+
   width: 394px;
   height: 368px;
-  background-color: #fff;
-  border: 1px solid #ececec;
   border-radius: 6px;
   overflow: hidden;
+  background-color: #fff;
+  border: 1px solid #ececec;
   box-shadow: 0 2px 10px rgba(#000, .06);
 }
 
