7362f69027320032b97874a77572b67f4cffb2ac
d36choi <yuio1126@gmail.com>
2021-01-27 11:32:51 +0900

    feat: Create lazyLoad.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from './App'
 import store from './store'
 import searchData from './plugins/searchData'
 import fetchData from "./plugins/fetchData";
+import lazyLoad from "./plugins/lazyLoad";
+
+Vue.use(lazyLoad)
 Vue.use(searchData)
 Vue.use(fetchData)
 new Vue({
