45acc47c78271a78c1bdc3c958c638e97d65327d
d36choi <yuio1126@gmail.com>
2021-01-27 13:49:14 +0900

    feat: Add styles of ProductCard.vue
    TODO
    count and shipping styles

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,10 +1,13 @@
 <template>
   <a
     class="product-card">
-    <div></div>
+    <div
+      :class="{ loading: imageLoading }"
+      class="thumbnail"
+      :style="{ backgroundImage: `url(${product.thumbnail})` }"></div>
     <div class="contents">
-      <div class="flag"></div>
-      <div class="name"></div>
+      <div class="flag"> {{ product.flag }}</div>
+      <div class="name">{{ product.name }}</div>
       <div class="prices">
         <div
           v-if="computedDiscountRate"
@@ -15,18 +18,25 @@
           <span class="unit">%</span>
         </div>
         <div class="price">
-          <span class="value"></span>
-          <span class="unit"></span>
+          <span class="value"> {{ product.price.value }}</span>
+          <span class="unit"> {{ product.price.unit }}</span>
         </div>
-        <div class="list-price">
-          <span class="value"></span>
-          <span class="unit"></span>
+        <div
+          class="list-price"
+          v-if="product.listPrice">
+          <span class="value"> {{ product.listPrice.value }}</span>
+          <span class="unit"> {{ product.listPrice.unit }}</span>
         </div>
-        <div class="options">
-          <div class="free-shipping"></div>
-          <div class="accumulation"><span></span></div>
+        <div
+          v-if="product.options.shipping"
+          class="options">
+          <div class="free-shipping">{{ product.options.shipping }}</div>
+          <div class="accumulation"><span v-html="product.options.accumulation"></span></div>
           <span class="flex-space"></span>
-          <div class="count"></div>
+          <div
+            v-if="product.options.count"
+            class="count">
+            {{ product.options.count }}</div>
         </div>
 
       </div>
@@ -96,6 +106,80 @@ export default {
   background-color: #fff;
   border: 1px solid #ececec;
   box-shadow: 0 2px 10px rgba(#000, .06);
+  .thumbnail {
+    height: 198px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    &.loading {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #f4f4f4;
+      &::after {
+        content: "";
+        width: 94px;
+        height: 40px;
+        margin: 0 24px;
+        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+        background-position: -162px 0;
+        background-size: 363px;
+        filter: grayscale(100%);
+        opacity: .2;
+      }
+    }
+  }
+  .contents {
+    padding: 20px 20px 0;
+    height: 120px;
+    box-sizing: border-box;
+    position: relative;
+    .flag {
+      display: inline-block;
+      padding: 0 4px;
+      height: 22px;
+      line-height: 22px;
+      font-size: 12px;
+      color: #f43142;
+      background-color: #FFF8F8;
+      border: 1px solid #f43142;
+      position: absolute;
+      top: -11px;
+    }
+    .name {
+      font-size: 22px;
+      white-space: nowrap;
+    }
+    .prices {
+      display: flex;
+      align-items: baseline;
+      .discount-rate {
+        display: flex;
+        align-items: baseline;
+        margin-right: 10px;
+        color: #f43142;
+        .value {
+          font-size: 24px;
+        }
+      }
+      .price {
+        display: flex;
+        align-items: baseline;
+        margin-right: 10px;
+        .value {
+          font-size: 24px;
+          font-weight: 900;
+        }
+      }
+      .list-price {
+        display: flex;
+        align-items: baseline;
+        color: #999;
+        text-decoration-line: line-through;
+      }
+    }
+
+  }
 }
 
 </style>
